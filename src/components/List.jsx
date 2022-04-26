fbcf9e77a83f7ed49b9b772681f2060b0d5e5fd5
joshua360x <jkw5499@psu.edu>
2022-04-26 19:44:23 -0400

    took out dummy data for real api call

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -10,22 +10,22 @@ export default function List() {
   const [search, setSearch] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
-  const data = [
-    {
-      description: 'food is goood',
-      name: 'big burger',
-      ingredients: ['beef', 'ham', 'lettuce']
-    },
-    {
-      description: 'food is bad',
-      name: 'big fish burger',
-      ingredients: ['fish', 'tureky', 'tomato']
-    }
-  ]
-  useEffect(() => {
-    setBurgers(data)
+  // const data = [
+  //   {
+  //     description: 'food is goood',
+  //     name: 'big burger',
+  //     ingredients: ['beef', 'ham', 'lettuce']
+  //   },
+  //   {
+  //     description: 'food is bad',
+  //     name: 'big fish burger',
+  //     ingredients: ['fish', 'tureky', 'tomato']
+  //   }
+  // ]
+  // useEffect(() => {
+  //   setBurgers(data)
 
-  }, [])
+  // }, [])
 
   function searchHandler(e) {
     setSearch(e.target.value)
@@ -35,27 +35,28 @@ export default function List() {
     setFilteredResults(filteredBurgers);
   }
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   // .then(response => response.json())
-  //   // .then(response => console.log(response))
-  //   // .catch(err => console.error(err));
-  //   async function getBurger() {
-  //     const options = {
-  //       method: 'GET',
-  //       headers: {
-  //         'X-RapidAPI-Host': 'burgers1.p.rapidapi.com',
-  //         'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
-  //       }
-  //     };
-  //     const displayBurger = await fetch('https://burgers1.p.rapidapi.com/burgers', options)
-  //     const json = await displayBurger.json();
-  //     console.log('json :>> ', json);
-  //   };
-  //   setIsLoading(false)
-  //   getBurger();
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
+    async function getBurger() {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': 'burgers1.p.rapidapi.com',
+          'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
+        }
+      };
+      const displayBurger = await fetch('https://burgers1.p.rapidapi.com/burgers', options)
+      const json = await displayBurger.json();
+      // console.log('json :>> ', json);
+      setBurgers(json)
+    };
+    setIsLoading(false)
+    getBurger();
   
-  // }, [])
+  }, [])
   return (
     <>
     {/* { isLoading ? (<h3> Hold On We are Loading your burgers</h3> ) : ( */}
